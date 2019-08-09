068ffbdaf2e5bb2e37f313f7096a79e631ec0d7e
Thomas Conner <thomas.conner@me.com>
2019-08-09 12:30:05 -0400

    Bind to 0.0.0.0

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ fastify.get('/', async () => {
 // Run the server!
 async function start() {
   try {
-    await fastify.listen(PORT);
+    await fastify.listen(PORT, '0.0.0.0');
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
